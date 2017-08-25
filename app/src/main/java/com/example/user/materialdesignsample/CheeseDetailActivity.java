0b5c5ca20ae3d071e5e8e1087d2b7bf3ea064eda
shubham chauhan <shubham.chauhan@kelltontech.com>
2017-08-25 11:38:40 +0530

    + some changes

--- app/src/main/java/com/example/user/materialdesignsample/CheeseDetailActivity.java
+++ app/src/main/java/com/example/user/materialdesignsample/CheeseDetailActivity.java
@@ -1,26 +1,38 @@
 package com.example.user.materialdesignsample;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 public class CheeseDetailActivity extends AppCompatActivity {
     public static final String EXTRA_NAME = "cheese_name";
+    private ImageView mImageViewSmall;
+    private ImageView mImageViewLarge;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheese_detail);
+// set an exit transition
 
         Intent intent = getIntent();
         final String cheeseName = intent.getStringExtra(EXTRA_NAME);
+        mImageViewSmall = (ImageView) findViewById(R.id.backdrop);
+        mImageViewLarge = (ImageView) findViewById(R.id.imageView_large);
 
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -30,12 +42,28 @@ public class CheeseDetailActivity extends AppCompatActivity {
                 (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle(cheeseName);
 
+        mImageViewSmall.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                Glide.with(CheeseDetailActivity.this).load(Cheeses.getRandomCheeseDrawable()).centerCrop().into(mImageViewLarge);
+                showReveal();
+            }
+        });
+
+        mImageViewLarge.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                hideReveal();
+            }
+        });
         loadBackdrop();
     }
 
     private void loadBackdrop() {
-        final ImageView imageView = (ImageView) findViewById(R.id.backdrop);
-        Glide.with(this).load(Cheeses.getRandomCheeseDrawable()).centerCrop().into(imageView);
+        Glide.with(this).load(Cheeses.getRandomCheeseDrawable()).centerCrop().into(mImageViewSmall);
+//        Glide.with(this).load(Cheeses.getRandomCheeseDrawable()).centerCrop().into(mImageViewLarge);
     }
 
     @Override
@@ -52,5 +80,55 @@ public class CheeseDetailActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    void showReveal() {
+        View myView = findViewById(R.id.imageView_large);
+
+// get the center for the clipping circle
+        int cx = myView.getWidth() / 2;
+        int cy = myView.getHeight() / 2;
+
+// get the final radius for the clipping circle
+        float finalRadius = (float) Math.hypot(cx, cy);
+
+// create the animator for this view (the start radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
+
+// make the view visible and start the animation
+        myView.setVisibility(View.VISIBLE);
+        anim.start();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    void hideReveal() {
+        final View myView = findViewById(R.id.imageView_large);
+
+// get the center for the clipping circle
+        int cx = myView.getWidth() / 2;
+        int cy = myView.getHeight() / 2;
+
+// get the initial radius for the clipping circle
+        float initialRadius = (float) Math.hypot(cx, cy);
+
+// create the animation (the final radius is zero)
+        Animator anim =
+                ViewAnimationUtils.createCircularReveal(myView, cx, cy, initialRadius, 0);
+
+// make the view invisible when the animation is done
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                myView.setVisibility(View.GONE);
+            }
+        });
+
+// start the animation
+        anim.start();
+
+
     }
 }
